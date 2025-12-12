50a6997d21d1305413587a1671143af771fa3b67
merruben28 <mruben@acxiom.com>
2025-12-13 00:10:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,15 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
-
-ingredients_string = ''
-for fruit_chosen in ingredients_list:
-  ingredients_string += fruit_chosen + ''
+  ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ''
 
 # st.write(ingredients_string)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order )
